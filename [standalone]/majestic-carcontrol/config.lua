b7370ba4f2d3c698b8cf59f989f08a12d19a8041
L1quidSn4ke <blue_lambergini@hotmail.com>
2025-08-29 12:04:20 -0700

    add carcontrol

---
+++ [standalone]/majestic-carcontrol/config.lua
@@ -0,0 +1,10 @@
+-- Configure to your liking
+
+Config = {
+    Menu = {
+        Command = "cc", -- In the F8 Menu use this Bind Keyboard "anybutton" "cc"
+        Align = "bottom-center", -- top-center, bottom-center, top-left, top-right, bottom-left, bottom-right
+        StyleType = "colored", -- colored, default
+    },
+    AlarmDuration = 30000, -- Second x 10000
+}
