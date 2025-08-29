bca1a99bde9c5490e9727a74ad9c1f6af037c6c4
L1quidSn4ke <blue_lambergini@hotmail.com>
2025-08-29 11:56:56 -0700

    add tunres mlo

---
+++ [maps]/np_tuners/fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+game 'gta5'
+this_is_a_map 'yes'
+files {
+	'common/*.meta'
+}
+data_file 'DLC_ITYP_REQUEST' 'stream/denis3d_tuners_metadata_001.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/denis3d_tunershop_int.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/denis3d_tunershop_props.ytyp'
